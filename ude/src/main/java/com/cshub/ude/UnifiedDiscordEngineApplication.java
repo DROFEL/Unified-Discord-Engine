31248d2bb4b2811c8adb06257a29854daf694074
Zhenxu Wang <mason123wp@gmail.com>
2023-10-25 21:19:03 -0400

    init spring boot

---
+++ ude/src/main/java/com/cshub/ude/UnifiedDiscordEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cshub.ude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedDiscordEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedDiscordEngineApplication.class, args);
+	}
+
+}
---
+++ ude/src/test/java/com/cshub/ude/UnifiedDiscordEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cshub.ude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedDiscordEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
